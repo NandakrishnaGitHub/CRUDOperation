a8316be9b289a7782f06518b8d7916f88ae22a4f
nhrudregowda <hrnandakrishna@gmail.com>
2024-08-08 07:22:35 +0530

    Commit

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
@@ -3,7 +3,7 @@ package com.luv2code.springboot.cruddemo.dao;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
 import java.util.List;
-
+//methods
 public interface EmployeeDAO {
 
    List<Employee> findAll();
